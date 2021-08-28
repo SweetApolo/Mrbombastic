68e3e7da6ada9757374ccfa601ad4dab10f2256b
Juan J Vanegas Paniagua <boovanegas@gmail.com>
2021-08-28 09:46:32 -0500

    Tareas agregadas

--- MrBombastic.ino
+++ MrBombastic.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup(){
+  
+  Serial.begin(115200);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+//Tarea 1 que va a controlar toda la bomba, modo config y modo activada
+void task1(){
+  
+}
+
+//Codigo clasico para tecnica no bloqueante
+void task2(){
+  
+}
+
+void loop(){
+  task1();
+  task2();
 }
