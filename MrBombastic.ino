4e54f854836bd295835c1174a2d8d48d3ed2355b
Juan J Vanegas Paniagua <boovanegas@gmail.com>
2021-08-28 10:29:52 -0500

    Funcion Bool claves

--- MrBombastic.ino
+++ MrBombastic.ino
@@ -4,6 +4,21 @@ void setup(){
   
 }
 
+bool clavesIguales(char *claveRecibida,char *claveCorrecta){
+  int ok = 0;
+
+  for (int i = 0; i < 6; i++){
+    if (claveRecibida[i] == claveCorrecta[i]){
+      ok++;
+    }
+  }
+  if (ok == 6){
+    return true; //la clave recibida es igual a la clave almacenada
+  }
+  else{
+    return false; //las claves no tan iguales
+  }
+}
 
 //Tarea 1 que va a controlar toda la bomba, modo config y modo activada
 void task1(){
